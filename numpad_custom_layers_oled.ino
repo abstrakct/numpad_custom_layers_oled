6a5e64778129ee6b3e9dbb8fd39a895d7facb426
Rolf K <abstractstar@gmail.com>
2017-07-11 19:32:14 +0200

    Removed unnecessary include.

--- numpad_custom_layers_oled.ino
+++ numpad_custom_layers_oled.ino
@@ -7,13 +7,6 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 
-#ifdef U8X8_HAVE_HW_SPI
-#include <SPI.h>
-#endif
-#ifdef U8X8_HAVE_HW_I2C
-#include <Wire.h>
-#endif
-
 
 // U8G2 constructor
 U8G2_SSD1306_128X32_UNIVISION_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ 2, /* data=*/ 14, /* reset=*/ U8X8_PIN_NONE);
